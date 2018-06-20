38f7b6ab9b16ad1f978b898ad2036d4008163d29
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-06-20 14:55:01 +0200

    Some shortcuts

--- home/.common.sh
+++ home/.common.sh
@@ -3,9 +3,15 @@
 ############################################
 
 [ -d $HOME/bin ] && PATH=$PATH:$HOME/bin
+if which ruby >/dev/null && which gem >/dev/null; then
+    PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+fi
 
 EDITOR=zile
 VISUAL=emacs
+export HOMEBREW_EDITOR=zile
+export HOMEBREW_GITHUB_API_TOKEN="cb231b26300206a8df32431412e00fdd41db138a"
+export JULIA_NIGHTLY="/Applications/Julia-0.7.app/Contents/Resources/julia/bin/julia"
 
 HISTCONTROL=ignoredups
 HISTCONTROL=ignoreboth
@@ -24,6 +30,13 @@ export GCC_COLORS=1
 alias rsyncaz='rsync -az --info=progress2'
 alias rsyncrz='rsync -rlDz --info=progress2'
 alias tb="nc termbin.com 9999"
+alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
+alias e="emacsclient --no-wait"
+alias getpage='wget --wait=2 --no-parent -r -p -k'
+
+if type imgcat >/dev/null 2>&1; then
+    alias qrpaste="pbpaste | qrencode -o - | imgcat"
+fi
 
 ############################################
 # Functions
@@ -39,4 +52,54 @@ function notebookserver {
 
 function ipythonqt {
     ipython3 qtconsole --matplotlib=inline --IPythonQtConsoleApp.hide_menubar=True
+}
+
+function lypwatch {
+    lilypond "$1.ly"
+    # open -a skim "$1.pdf" 2> /dev/null &
+    e "$1.pdf"
+    e "$1.ly"
+    lyp watch "$1.ly"
+}
+
+function syncthing_remote {
+    ssh -L "${2}:localhost:${3}" $1 &
+    open http://localhost:$2
+    sleep 0.1
+    fg
+}
+
+function syncthing_home {
+    syncthing_remote home 9090 8080
+}
+
+function prtar {
+    [ $# -ne 2 ] && echo "Usage $0 <archive file> <directory>" && return 1
+    extension="${1##*.}"
+    case $extension in
+        gz)
+            compr=gzip
+            ;;
+        bz2)
+            compr=bzip2
+            ;;
+        xz)
+            compr=xz
+            ;;
+        *)
+            echo "Unknown archive format, $extension" && return 1
+            ;;
+    esac
+    tar cf - $2 | pv -s $(gdu -sb $2 | awk '{print $1}') | $compr > $1
+}
+
+function osc {
+    rm -f $1.hpgl
+    readtty /dev/tty.usbserial ";DF;" | pv > $1.hpgl
+    rm -f $1.eps
+    cat $1.hpgl | hp2xx -q -f- -m eps -a 2 > $1.eps
+}
+
+function pip-upgrade {
+    pip install $(pip list --outdated --format=columns | tail -n +3 | awk '{print $1 }') --upgrade
 }
